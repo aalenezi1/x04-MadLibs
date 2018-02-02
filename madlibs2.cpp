25e4c3ca5833f06297d8cea638287292c53d5191
aalenezi1 <aalenezi1@mail.csuchico.edu>
2018-02-01 21:12:34 -0800

    making a story in c language

---
+++ madlibs2.cpp
@@ -0,0 +1,30 @@
+//Author: Abdallah Alenezi
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string place;
+	int age;
+	int time;
+	char character;
+
+cout<< "how old was I?\n";
+cin>> age;
+
+cout<< "where did you go?\n";
+cin>> place;
+
+cout<< "when did you arrived?\n";
+cin>> time;
+
+cout<< "what type of character you used in your story?\n";
+cin>> character;
+
+cout<< "when i was "<<age<<" years old, i went to the "<<place<<". i arrived at "<<time<<"A.M. i asked the adviseor what class should i take "<<character<<"\n";
+
+	return 0;
+}
